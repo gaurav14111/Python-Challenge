3a3e127fbbd5393922064a86c2d352c35f8e866f
Gaurav Maheshwari <gaurav.ucberkley@gmail.com>
2019-03-07 18:54:22 -0800

    updated
    updated

--- PyBank/main.py
+++ PyBank/main.py
@@ -26,11 +26,25 @@ with open(csvpath, newline="") as csvfile:
         iprofit_loss = [int(i) for i in profit_loss]
         
 Average_Change = sum(iprofit_loss) / len(iprofit_loss)
+
+#Finding the index value of max of iprofit_loss list to find the index and find date
+max_value = max(iprofit_loss)
+max_index = iprofit_loss.index(max_value)
+max_date = date[max_index]
+
+
+#Finding the index value of min of iprofit_loss list to find the index and find date
+min_value = min(iprofit_loss)
+min_index = iprofit_loss.index(min_value)
+min_date = date[min_index]
+
+
+
 print("Financial Analysis")
 print("--------------------------------")
 print (f"Total Months: {len(date)}")
 print (f"Total: ${sum(iprofit_loss)}")
 print(f"Average Change: ${Average_Change}")
-print(f"Greatest Increase in Profits: (${max(iprofit_loss)})")
-print(f"Greatest Decrease in Profits: (${min(iprofit_loss)})")
+print(f"Greatest Increase in Profits: {max_date} (${max_value})")
+print(f"Greatest Decrease in Profits: {min_date} (${min_value})")
             
